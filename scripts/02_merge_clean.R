4260e0a86a145e86437bf368de1c1a120bcbca68
BigMikeZ <mikeiszhang@gmail.com>
2026-05-19 18:18:21 -0600

    Merge and start cleaning data

--- scripts/02_merge_clean.R
+++ scripts/02_merge_clean.R
@@ -8,7 +8,7 @@ file_pattern <- c(
   PAQ   = "^PAQ",
   HUQ   = "^HUQ",
   SMQ   = "^SMQ",
-  ALQ   = "ALQ"
+  ALQ   = "^ALQ"
 )
 
 files <- map(file_pattern, function(pat) {
@@ -19,10 +19,12 @@ str(files)
 file_list <- map(files, function(f) {
   map_df(f, function(f) {
     read_rds(f) |> 
-      mutate(across(everything(), as.character))
+      mutate(
+        cycle = str_extract(f, "[HIJ](?=\\.rds)"),
+        across(everything(), as.character)
+      )
   })
 })
-str(file_list)
 
 nhanes_merge <- file_list$DEMO |> 
   left_join(file_list$DPQ, join_by("SEQN")) |> 
@@ -33,5 +35,49 @@ nhanes_merge <- file_list$DEMO |>
   left_join(file_list$SMQ, join_by("SEQN")) |> 
   left_join(file_list$ALQ, join_by("SEQN")) 
 
-nrow(nhanes_merge)
+str(nhanes_merge)
+
+nhanes_merge <- nhanes_merge |> 
+  rename(cycle = cycle.x) |> 
+  select(-matches("cycle\\.y")) |> 
+  select(-matches("cycle\\.x"))
+nhanes_merge |> count(cycle)
 ncol(nhanes_merge)
+
+names(nhanes_merge)[names(nhanes_merge) %in% c("SLD012", "SLD010H")]
+
+nhanes_clean <- nhanes_merge |> 
+  select(
+    SEQN, cycle, WTMEC2YR, SDMVPSU, SDMVSTRA, DPQ010:DPQ090, 
+    RIDAGEYR, RIAGENDR, RIDRETH3, DMDEDUC2, INDFMPIR, FSDAD,
+    SLD010H, SLD012, PAQ605, PAQ620, HUQ090, SMQ020, SMQ040, 
+    ALQ130
+    ) |> 
+  mutate(
+    across(c(SEQN, WTMEC2YR, SDMVPSU, SDMVSTRA, RIDAGEYR, INDFMPIR,
+             SLD010H, SLD012, ALQ130), as.numeric)
+  ) |> 
+  mutate(
+    across(-c(SEQN, WTMEC2YR, SDMVPSU, SDMVSTRA, RIDAGEYR, INDFMPIR,
+              SLD010H, SLD012, ALQ130), factor)
+  ) |> 
+  mutate(
+    sleep_hr = coalesce(SLD010H, SLD012)
+  ) |> 
+  select(-c(SLD010H, SLD012))
+
+nhanes_clean |>
+  summarise(across(everything(), ~ mean(is.na(.)))) |>
+  pivot_longer(everything()) |>
+  arrange(desc(value)) |> 
+  print(n = 27)
+
+nhanes_clean |> count(RIDAGEYR < 18)
+
+nhanes_clean <- nhanes_clean |> 
+  filter(RIDAGEYR >= 18) 
+nhanes_clean |> 
+  summarise(across(everything(), ~ mean(is.na(.)))) |>
+  pivot_longer(everything()) |>
+  arrange(desc(value)) |> 
+  print(n = 27)
