c574e2a32f2f5a683e2540250ad7529f06c67e1c
BigMikeZ <mikeiszhang@gmail.com>
2026-05-25 16:03:02 -0600

    Build LASSO

--- scripts/04_modeling.R
+++ scripts/04_modeling.R
@@ -55,3 +55,74 @@ ggplot(check_data, aes(logit, predictor.value))+
 
 # Check influential values (cook's distance)
 plot(model_glm, which = 4, id.n = 3)
+
+# Build LASSO
+set.seed(123)
+nhanes_split <- initial_split(nhanes_clean, prop = 0.8, strata = depressed)
+nhanes_train <- training(nhanes_split)
+nhanes_test  <- testing(nhanes_split)
+
+set.seed(123)
+nhanes_folds <- vfold_cv(nhanes_train, v = 10, strata = depressed)
+
+lasso_recipe <- recipe(depressed ~ ., data = nhanes_train) |> 
+  update_role(SEQN, cycle, WTMEC2YR, SDMVPSU, SDMVSTRA, phq9_score,
+              new_role = "ID") |> 
+  step_normalize(all_numeric_predictors()) |>
+  step_dummy(all_nominal_predictors()) |>
+  step_zv(all_predictors())
+
+lasso_recipe |> prep() |> bake(new_data = NULL) |> glimpse()
+
+lasso_spec <- logistic_reg(
+  penalty = tune(),
+  mixture = 1
+) |>
+  set_engine("glmnet") |>
+  set_mode("classification")
+
+lasso_workflow <- workflow() |>
+  add_recipe(lasso_recipe) |>
+  add_model(lasso_spec)
+
+lambda_grid <- grid_regular(
+  penalty(range = c(-4, 0), trans = log10_trans()),
+  levels = 50
+)
+
+set.seed(123)
+lasso_tune <- tune_grid(
+  lasso_workflow,
+  resamples = nhanes_folds,
+  grid      = lambda_grid,
+  metrics   = metric_set(roc_auc)
+)
+
+autoplot(lasso_tune)
+show_best(lasso_tune, metric = "roc_auc")
+
+best_lambda <- select_by_one_std_err(lasso_tune, 
+                                     metric = "roc_auc",
+                                     desc(penalty))
+best_lambda
+
+final_workflow <- lasso_workflow |>
+  finalize_workflow(best_lambda)
+
+final_fit <- final_workflow |>
+  fit(data = nhanes_train)
+
+final_fit |>
+  extract_fit_parsnip() |>
+  tidy() |>
+  filter(estimate != 0) |>
+  arrange(desc(abs(estimate)))
+
+last_fit_result <- final_workflow |>
+  last_fit(nhanes_split, metrics = metric_set(roc_auc))
+
+collect_metrics(last_fit_result)
+last_fit_result |>
+  collect_predictions() |>
+  roc_curve(truth = depressed, .pred_Yes, event_level = "second") |>
+  autoplot()
