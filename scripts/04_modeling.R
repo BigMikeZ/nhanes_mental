921dcfdc25c548eddfde1f5db3ee4a935a4e5d72
BigMikeZ <mikeiszhang@gmail.com>
2026-06-09 14:54:46 -0600

    Modify linearity diagnostic plots

--- scripts/04_modeling.R
+++ scripts/04_modeling.R
@@ -15,35 +15,6 @@ nhanes_svy_model <- svydesign(
   data   = nhanes_clean
 ) 
 
-# Check linearity assumption (logit outcome on predictors)
-model_glm <- glm(
-  depressed ~ RIDAGEYR + INDFMPIR + ALQ130 + sleep_hr +
-    RIAGENDR + RIDRETH3 + dmdeduc2_collapsed +
-    fsdad_recoded + activity + smoking_status,
-  data   = nhanes_clean,
-  family = binomial()
-)
-
-probabilities <- predict(model_glm, type = "response")
-
-nhanes_check <- nhanes_clean |> 
-  select(RIDAGEYR, sleep_hr, INDFMPIR, ALQ130)
-
-predictors <- colnames(nhanes_check)
-
-check_data <- nhanes_check |> 
-  mutate(logit = log(probabilities/(1 - probabilities))) |> 
-  pivot_longer(-logit, names_to = "predictors", values_to = "predictor.value")
-
-ggplot(check_data, aes(logit, predictor.value))+
-  geom_point(size = 0.5, alpha = 0.5) +
-  geom_smooth(method = "loess") + 
-  theme_bw() + 
-  facet_wrap(~predictors, scales = "free_y")
-
-# Check influential values (cook's distance)
-plot(model_glm, which = 4, id.n = 3)
-
 #Build logistic regression model
 model_logistic <- svyglm(
   depressed ~ RIDAGEYR + RIAGENDR + RIDRETH3 + dmdeduc2_collapsed +
@@ -62,7 +33,7 @@ tbl_regression(
     RIAGENDR                    ~ "Sex",
     RIDRETH3                    ~ "Race/Ethnicity",
     dmdeduc2_collapsed          ~ "Education",
-    `log(INDFMPIR + 0.01)`      ~ "Income-to-poverty ratio (log)",
+    `log(INDFMPIR + 0.01)`      ~ "Income-to-poverty ratio",
     fsdad_recoded               ~ "Food Security",
     activity                    ~ "Physical Activity",
     smoking_status              ~ "Smoking Status",
@@ -72,6 +43,28 @@ tbl_regression(
   ) |> 
   as_gt() |> 
   gt::gtsave("output/tables/logistic_summary.html")
+
+# Check linearity assumption (logit outcome on predictors)
+probabilities <- predict(model_logistic, type = "response")
+
+nhanes_check <- nhanes_clean |> 
+  mutate(log_INDFMPIR = log(INDFMPIR + 0.01)) |>
+  select(RIDAGEYR, sleep_hr, log_INDFMPIR, ALQ130)
+
+predictors <- colnames(nhanes_check)
+
+check_data <- nhanes_check |> 
+  mutate(logit = log(probabilities/(1 - probabilities))) |> 
+  pivot_longer(-logit, names_to = "predictors", values_to = "predictor.value")
+
+ggplot(check_data, aes(predictor.value, logit))+
+  geom_point(size = 0.5, alpha = 0.5) +
+  geom_smooth(method = "loess") + 
+  theme_bw() + 
+  facet_wrap(~predictors, scales = "free_x")
+
+# Check influential values (cook's distance)
+plot(model_logistic, which = 4, id.n = 3)
 
 # Build LASSO
 set.seed(123)
